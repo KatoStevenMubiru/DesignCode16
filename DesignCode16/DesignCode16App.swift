890cc14b354523b2841d93b59f383990e72c766c
Kato Steven Mubiru <kato.steven60@gmail.com>
2023-02-25 17:55:09 +0300

    Initial Commit

---
+++ DesignCode16/DesignCode16App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCode16App.swift
+//  DesignCode16
+//
+//  Created by Kato Steven Mubiru on 25/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct DesignCode16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
