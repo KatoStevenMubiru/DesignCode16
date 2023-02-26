624b9bf0a0e7ff35f315e6bf02ad0ba203e8e834
Kato Steven Mubiru <kato.steven60@gmail.com>
2023-02-26 06:12:22 +0300

    Commit2

---
+++ DesignCode16/NavigationStackView.swift
@@ -0,0 +1,23 @@
+//
+//  NavigationStackView.swift
+//  DesignCode16
+//
+//  Created by Kato Steven Mubiru on 26/02/2023.
+//
+
+import SwiftUI
+
+struct NavigationStackView: View {
+    var body: some View {
+        
+        
+        Text("Item")
+    
+    }
+}
+
+struct NavigationStackView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationStackView()
+    }
+}
