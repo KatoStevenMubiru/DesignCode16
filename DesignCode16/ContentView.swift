fbfb3eb06e723617b9b858ee29cfd88b5341672a
Kato Steven Mubiru <kato.steven60@gmail.com>
2023-02-27 09:05:28 +0300

    Update ContentView.swift

--- DesignCode16/ContentView.swift
+++ DesignCode16/ContentView.swift
@@ -52,5 +52,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .background(Image("Wallpaper 2"))
     }
 }
