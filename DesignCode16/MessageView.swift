fcdd4ffc84a14821c29ef72fed7915da5b6c871e
Kato Steven Mubiru <kato.steven60@gmail.com>
2023-03-02 06:10:08 +0300

    2

---
+++ DesignCode16/MessageView.swift
@@ -0,0 +1,20 @@
+//
+//  MessageView.swift
+//  DesignCode16
+//
+//  Created by Kato Steven Mubiru on 02/03/2023.
+//
+
+import SwiftUI
+
+struct MessageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MessageView_Previews: PreviewProvider {
+    static var previews: some View {
+        MessageView()
+    }
+}
