c1f1997bf5bc724c86f3ea992a8c5c7e02f6ef4d
Kato Steven Mubiru <kato.steven60@gmail.com>
2023-03-03 05:26:24 +0300

    Update MenuView.swift

--- DesignCode16/MenuView.swift
+++ DesignCode16/MenuView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct MenuView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List(navigationItems){ item in
+            Label(item.title, systemImage: ite)
+            
+        }
     }
 }
 
