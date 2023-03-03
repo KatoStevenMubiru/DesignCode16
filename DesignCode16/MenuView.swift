05a8a69a483151e41580c07728dc6d3c1139e9c4
Kato Steven Mubiru <kato.steven60@gmail.com>
2023-03-03 05:24:55 +0300

    MenuView

---
+++ DesignCode16/MenuView.swift
@@ -0,0 +1,20 @@
+//
+//  MenuView.swift
+//  DesignCode16
+//
+//  Created by Kato Steven Mubiru on 03/03/2023.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuView()
+    }
+}
