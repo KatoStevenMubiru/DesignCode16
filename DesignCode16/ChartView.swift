4872ebe8cb935c403d78dc7b5dd378a242bc29b7
Kato Steven Mubiru <kato.steven60@gmail.com>
2023-03-04 05:30:47 +0300

    Update ChartView.swift

--- DesignCode16/ChartView.swift
+++ DesignCode16/ChartView.swift
@@ -14,12 +14,17 @@ struct ChartView: View {
     var body: some View {
         
         ZStack {
-            Image("Wallpaper 3")
+            //Image("Wallpaper 3").ignoresSafeArea(.all)
             Chart {
                 
                 BarMark(x: .value("Day ", "June 1"), y:.value("Value", 50))
                         
                 BarMark(x: .value("Day ", "June 2"), y:.value("Value", 30))
+                    .foregroundStyle(.green)
+                
+                BarMark(x: .value("Day ", "June 3"), y:.value("Value", 20))
+                    .foregroundStyle(.yellow)
+                
                         
                 
             }// chart
