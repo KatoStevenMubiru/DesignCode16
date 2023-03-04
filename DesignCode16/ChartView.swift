2cd00a9150d6ff07e8d33814039b9c733fc4fdbf
Kato Steven Mubiru <kato.steven60@gmail.com>
2023-03-04 05:19:59 +0300

    chartView

---
+++ DesignCode16/ChartView.swift
@@ -0,0 +1,20 @@
+//
+//  ChartView.swift
+//  DesignCode16
+//
+//  Created by Kato Steven Mubiru on 04/03/2023.
+//
+
+import SwiftUI
+
+struct ChartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChartView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChartView()
+    }
+}
