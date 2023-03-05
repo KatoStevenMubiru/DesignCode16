2795157ad2d673a6fea2d347059f0ad9bb1e6db6
Kato Steven Mubiru <kato.steven60@gmail.com>
2023-03-05 07:21:36 +0300

    Update DetailView.swift

--- DesignCode16/DetailView.swift
+++ DesignCode16/DetailView.swift
@@ -9,7 +9,13 @@ import SwiftUI
 
 struct DetailView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ChartView()
+            CardView()
+            GridView()
+        }//Vstack
+      
+        
     }
 }
 
