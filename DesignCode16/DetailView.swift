ade56b586c6ead92c3ce507d336259353d66651b
Kato Steven Mubiru <kato.steven60@gmail.com>
2023-03-05 07:19:05 +0300

    DetailView

---
+++ DesignCode16/DetailView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailView.swift
+//  DesignCode16
+//
+//  Created by Kato Steven Mubiru on 05/03/2023.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}
