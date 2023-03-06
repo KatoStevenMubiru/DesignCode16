a838a918c0804710a501c2ba299d2fb3c1c4fa7a
Kato Steven Mubiru <kato.steven60@gmail.com>
2023-03-06 06:14:24 +0300

    Update RadialLayoutView.swift

--- DesignCode16/RadialLayoutView.swift
+++ DesignCode16/RadialLayoutView.swift
@@ -34,7 +34,7 @@ struct customLayOut: Layout{
     }
     
     func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) {
-        for(index, subview)
+        for(index, subview) in subviews.enumerated()
     }
     
     
