6ba329a17646bcf58743dafcf533cc8bc51f75c6
Kato Steven Mubiru <kato.steven60@gmail.com>
2023-03-06 06:03:10 +0300

    Update RadialLayoutView.swift

--- DesignCode16/RadialLayoutView.swift
+++ DesignCode16/RadialLayoutView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct RadialLayoutView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Circle()
+            .frame(width: 44)
+        
     }
 }
 
