e94ee2797923d35af2046adb0c56ec931d7a5e51
Kato Steven Mubiru <kato.steven60@gmail.com>
2023-03-08 06:10:44 +0300

    actionButtonView

---
+++ DesignCode16/actionButtonView.swift
@@ -0,0 +1,27 @@
+//
+//  actionButtonView.swift
+//  DesignCode16
+//
+//  Created by Kato Steven Mubiru on 08/03/2023.
+//
+
+import SwiftUI
+
+struct actionButtonView: View {
+    var body: some View {
+        
+        ZStack {
+            Image("UI 1")
+                .resizable()
+            .cornerRadius(50)
+            
+        }//Zstack
+        .background(.black)
+    }
+}
+
+struct actionButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        actionButtonView()
+    }
+}
