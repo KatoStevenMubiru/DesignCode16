bc58aece738ce7e4be79b37f44acd16c4c10de20
Kato Steven Mubiru <kato.steven60@gmail.com>
2023-03-09 21:26:46 +0300

    Update compactView.swift

--- DesignCode16/compactView.swift
+++ DesignCode16/compactView.swift
@@ -13,11 +13,13 @@ struct compactView: View {
         
         RadialGradient(colors: [Color(#colorLiteral(red: 0.2980392157, green: 0.3058823529, blue: 0.4431372549, alpha: 1)), .black], center: .center, startRadius: 1, endRadius: 400)
             .ignoresSafeArea(.all)
+        
+        Circle()
     }//body
     
     var gradient : LinearGradient{
         
-      LinearGradient(colors: <#T##[Color]#>, startPoint: <#T##UnitPoint#>, endPoint: <#T##UnitPoint#>)
+        LinearGradient(colors: [Color(#colorLiteral(red: 0.6196078431, green: 0.6274509804, blue: 0.7098039216, alpha: 1)), .black], startPoint: .topTrailing, endPoint: .bottomLeading)
         
         
     }// gradient
