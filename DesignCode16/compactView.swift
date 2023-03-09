07d4aba4b69657d3af9e1a1c6edda77117369e5f
Kato Steven Mubiru <kato.steven60@gmail.com>
2023-03-09 21:01:29 +0300

    compactView

---
+++ DesignCode16/compactView.swift
@@ -0,0 +1,20 @@
+//
+//  compactView.swift
+//  DesignCode16
+//
+//  Created by Kato Steven Mubiru on 09/03/2023.
+//
+
+import SwiftUI
+
+struct compactView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct compactView_Previews: PreviewProvider {
+    static var previews: some View {
+        compactView()
+    }
+}
