0ed15e43d270321a9cbe11304d61b64fbfe6f97c
Kato Steven Mubiru <kato.steven60@gmail.com>
2023-03-09 21:32:27 +0300

    Update compactView.swift

--- DesignCode16/compactView.swift
+++ DesignCode16/compactView.swift
@@ -11,8 +11,10 @@ struct compactView: View {
     
     var body: some View {
         
-        RadialGradient(colors: [Color(#colorLiteral(red: 0.2980392157, green: 0.3058823529, blue: 0.4431372549, alpha: 1)), .black], center: .center, startRadius: 1, endRadius: 400)
-            .ignoresSafeArea()
+        ZStack {
+            RadialGradient(colors: [Color(#colorLiteral(red: 0.2980392157, green: 0.3058823529, blue: 0.4431372549, alpha: 1)), .black], center: .center, startRadius: 1, endRadius: 400)
+                .ignoresSafeArea()
+        }//Zstack
         
         Circle().strokeBorder(gradient, style: StrokeStyle(lineWidth: 5, dash: [1, 1]))
         Circle().strokeBorder(gradient, style: StrokeStyle(lineWidth: 10, dash: [1, 6]))
