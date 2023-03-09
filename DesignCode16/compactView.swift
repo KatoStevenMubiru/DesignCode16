108c4ab186d6d65728201b132515c9554e704aa3
Kato Steven Mubiru <kato.steven60@gmail.com>
2023-03-09 21:21:15 +0300

    Update compactView.swift

--- DesignCode16/compactView.swift
+++ DesignCode16/compactView.swift
@@ -8,11 +8,10 @@
 import SwiftUI
 
 struct compactView: View {
-    
-     
+         
     var body: some View {
         
-        RadialGradient(colors: [Color("3B3C5E"), .black], center: .center, startRadius: 1, endRadius: 400)
+        RadialGradient(colors: [Color(#colorLiteral(red: 0.2980392157, green: 0.3058823529, blue: 0.4431372549, alpha: 1)), .black], center: .center, startRadius: 1, endRadius: 400)
     }
 }
 
