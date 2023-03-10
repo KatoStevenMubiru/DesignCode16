adbace9f5165457eec9ac42987cdd881498689ff
Kato Steven Mubiru <kato.steven60@gmail.com>
2023-03-10 06:30:43 +0300

    circleLabelView

---
+++ DesignCode16/circleLabelView.swift
@@ -0,0 +1,20 @@
+//
+//  circleLabelView.swift
+//  DesignCode16
+//
+//  Created by Kato Steven Mubiru on 10/03/2023.
+//
+
+import SwiftUI
+
+struct circleLabelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct circleLabelView_Previews: PreviewProvider {
+    static var previews: some View {
+        circleLabelView()
+    }
+}
