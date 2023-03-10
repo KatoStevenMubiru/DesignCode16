4939ef81305471674d75e0ea8c4604f89ac5b799
Kato Steven Mubiru <kato.steven60@gmail.com>
2023-03-10 06:36:26 +0300

    Update circleLabelView.swift

--- DesignCode16/circleLabelView.swift
+++ DesignCode16/circleLabelView.swift
@@ -8,8 +8,18 @@
 import SwiftUI
 
 struct circleLabelView: View {
+    
+    var text = "Latitude".uppercased()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack {
+            ForEach(Array(text.enumerated()), id: \.offset) { index, letter in
+            
+                    Text(String(letter))
+                
+            }
+        }//forEach
     }
 }
 
