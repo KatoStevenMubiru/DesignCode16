5c982f2001843566bc3ecf54ea0ed18bf4ffcb37
Kato Steven Mubiru <kato.steven60@gmail.com>
2023-03-10 05:52:58 +0300

    Update compactView.swift

--- DesignCode16/compactView.swift
+++ DesignCode16/compactView.swift
@@ -37,9 +37,14 @@ struct compactView: View {
     }//innerCircles
     
     
-    var innerCircle1
+    var innerCircle1 : some View{
+        
+        
+        
+        
+    }//innerCircle1
     
-    
+
     var background: some View{
         
         RadialGradient(colors: [Color(#colorLiteral(red: 0.2980392157, green: 0.3058823529, blue: 0.4431372549, alpha: 1)), .black], center: .center, startRadius: 1, endRadius: 400)
