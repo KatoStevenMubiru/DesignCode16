19e0cf0d59af3b39ddc61b5f2013cffc0eb01aad
Kato Steven Mubiru <kato.steven60@gmail.com>
2023-04-05 15:18:22 +0300

    Create customSheet.swift

---
+++ DesignCode16/customSheet.swift
@@ -0,0 +1,20 @@
+//
+//  customSheet.swift
+//  DesignCode16
+//
+//  Created by Kato Steven Mubiru on 05/04/2023.
+//
+
+import SwiftUI
+
+struct customSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct customSheet_Previews: PreviewProvider {
+    static var previews: some View {
+        customSheet()
+    }
+}
